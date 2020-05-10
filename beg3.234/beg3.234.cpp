a0713e9eb262e95233bddbcff13f31d774484fb5
Rodrigo Dias <rodrigo1406@gmail.com>
2020-05-10 20:29:39 -0300

    Add files via upload

--- beg3.234/beg3.234.cpp
+++ beg3.234/beg3.234.cpp
@@ -8,6 +8,41 @@
 
 using namespace std;
 
+osg::Vec3d fromQuat(const osg::Quat& quat, bool degrees)
+{
+    // From: http://guardian.curtin.edu.au/cga/faq/angles.html
+    // Except OSG exchanges pitch & roll from what is listed on that page
+    double qx = quat.x();
+    double qy = quat.y();
+    double qz = quat.z();
+    double qw = quat.w();
+
+    double sqx = qx * qx;
+    double sqy = qy * qy;
+    double sqz = qz * qz;
+    double sqw = qw * qw;
+
+    double term1 = 2 * (qx*qy + qw*qz);
+    double term2 = sqw + sqx - sqy - sqz;
+    double term3 = -2 * (qx*qz - qw*qy);
+    double term4 = 2 * (qw*qx + qy*qz);
+    double term5 = sqw - sqx - sqy + sqz;
+
+    double heading = atan2(term1, term2);
+    double pitch = atan2(term4, term5);
+    double roll = asin(term3);
+
+    //Return values in degrees if requested, else its radians
+    if(degrees)
+    {
+        heading = osg::RadiansToDegrees(heading);
+        pitch   = osg::RadiansToDegrees(pitch);
+        roll    = osg::RadiansToDegrees(roll);
+    }
+
+    return osg::Vec3d(heading, pitch, roll);
+}
+
 class ModelController : public osgGA::GUIEventHandler
 {
 public:
@@ -21,11 +56,21 @@ bool ModelController::handle( const osgGA::GUIEventAdapter& ea, osgGA::GUIAction
 {
 	if ( !_model )
 		return false;
+	/* Ângulos da câmera:
+	osg::Matrixd mx = view->getCamera()->getInverseViewMatrix();
+	osg::Vec3 hpr = fromQuat(mx.getRotate(),false);
+	h = osg::RadiansToDegrees(hpr.x());
+	p = osg::RadiansToDegrees(hpr.y())-90.0;
+	r = osg::RadiansToDegrees(hpr.z());*/
 	osg::Matrix matrix = _model->getMatrix();
+	osg::Vec3 hpr = fromQuat(matrix.getRotate(),true); // ângulos do Cessna, em graus
+	double h = hpr.x();
+	double p = hpr.y()-90.0;
+	double r = hpr.z();
 	switch ( ea.getEventType() )
 	{
 		case osgGA::GUIEventAdapter::KEYDOWN:
-			cout << (char)ea.getKey();
+			cout << (char)ea.getKey() << ": " << h << "|" << p << "|" << r << "\n";
 			switch ( ea.getKey() )
 			{
 				case 'a': case 'A':
